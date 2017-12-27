f13f1427e6f3d6a325ad1d308e8a705b2ee9dd0b
Techgoat <32556426+techgoat-net@users.noreply.github.com>
2017-12-27 11:23:22 +0100

    Update rdp-shadow.ps1
    Dialogfenster hinzugefügt, damit man den Benutzernamen nicht eintippen muss, sondern bequem per Mausklick auswaehlen kann.

--- rdp-shadow.ps1
+++ rdp-shadow.ps1
@@ -16,17 +16,53 @@ $Sessions=@()
     $objSession | Add-Member -MemberType NoteProperty -Name Type -Value $_.substring(1,3).trim()
     $objSession | Add-Member -MemberType NoteProperty -Name Username -Value $_.substring(19,22).trim()
     $objSession | Add-Member -MemberType NoteProperty -Name Status -Value $_.substring(48,8).trim()
-    if($objSession.Username -ne "" -and $objSession.Username -ne "USERNAME") {
+    if($objSession.Username -ne "" -and $objSession.Username -ne "USERNAME" -and $objSesson.Username -notlike $env:username) {
         $Sessions+=$objSession
     }
 }
-# Falls kein Benutzername per Parameter übergeben wurde, wird er hier erfragt (und die Liste aller vorhandenen ausgegeben)
+# Wir definieren eine globale Variable fuer den Benutzernamen wie folgt:
+$global:Username=$Username # Das brauchen wir naemlich fuer ein eigenes Dialogfenster.
+# Falls kein Benutzername per Parameter übergeben wurde, wird er hier erfragt (Buttons)
 if($Username -eq "") {
-    $Sessions | Sort-Object { $_.Username } | ft Username,Status,Type -autosize
-    $Username=Read-Host -Prompt "Please enter username to shadow on $ComputerName"
+    # Wir laden die Assemblies fuer eigene Formen / Fenster
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    
+    # Das Dialogfenster wird definiert
+    $objForm=New-Object System.Windows.Forms.Form
+    $objForm.Text="Select session to shadow"
+    $w=220
+    if($Sessions.Count -ge 3) {
+        $w=630
+    }
+    elseif($Sessions.Count -eq 2) {
+        $w=430
+    }
+    $x=0
+    $y=0
+    # Und wir gehen nun die Sitzungen durch und erstellen jeweils einen Button an entspr. Position
+    $Sessions | Sort-Object { $_.Username } | Foreach-Object {
+        if($x -ge 3) {
+            $y++
+            $x=0
+        }
+        $objBtn=New-Object System.Windows.Forms.Button
+        $objBtn.Location=New-Object System.Drawing.Size($(10+$x*200),$(10+$y*34))
+        $objBtn.Size=New-Object System.Drawing.Size(190,24)
+        $objBtn.Text=$_.Username
+        # Und wenn man drauf klickt, wird die globale Variable beschrieben
+        $objBtn.Add_Click({$global:Username=$this.Text;$objForm.Close()})
+        $objForm.Controls.Add($objBtn)
+        $x++
+    }
+    # Positionierung und Groesse des Dialogfensters
+    $objForm.Size=New-Object System.Drawing.Size($w,$(80+$y*34))
+    $objForm.StartPosition="CenterScreen"
+    # Dialogfenster anzeigen
+    [void] $objForm.ShowDialog()
 }
 $found=$false
-# Finde die Sitzung des eingegebenen Benutzernamens
+# Finde die Sitzung des ausgewaehlten Benutzernamens
 $Sessions | Where-Object { $_.Username -eq $Username } | Foreach-Object {
     $found=$true
     # Und spiegel die Sitzung, sofern sie aktiv ist.
